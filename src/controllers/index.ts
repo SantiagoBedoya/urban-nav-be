47058f85d19448b6d2787b5b1b336c3945b86d76
vnsaba <vanesa07alzate@gmail.com>
2023-09-30 11:12:39 -0500

    feat: notification model, repository, controller added

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,3 +7,4 @@ export * from './vehicle.controller';
 export * from './vehicle-user.controller';
 export * from './auth.controller';
 export * from './pqrs.controller';
+export * from './notification.controller';
