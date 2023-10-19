f0213cce32b17225f10ba3f3b1e33a2587fa2a16
Andrés Martínez <sbsmrth>
2023-10-18 23:09:08 -0500

    feat: best route method added

--- src/services/route.service.ts
+++ src/services/route.service.ts
@@ -3,6 +3,7 @@ import {BindingScope, injectable} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {CreateRouteRequest, PointsForDijkstra, RoutePoint} from '../models';
 import {RoutePointRepository, RouteRepository} from '../repositories';
+import {HttpErrors} from '@loopback/rest';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class RouteService {
@@ -11,7 +12,7 @@ export class RouteService {
     public routeRepository: RouteRepository,
     @repository(RoutePointRepository)
     private routePointRepository: RoutePointRepository,
-  ) { }
+  ) {}
 
   async create(route: CreateRouteRequest) {
     let totalDistance = 0;
@@ -48,29 +49,65 @@ export class RouteService {
   async getBestRoute(points: PointsForDijkstra) {
     const routes = await this.routeRepository.find({
       include: ['routePoints'],
-    })
+    });
 
-    const graph = new Graph()
+    const graph = new Graph();
 
-    const fullRoute = {};
+    const fullRoute: {[key: string]: string[]} = {};
 
     routes.forEach(route => {
       const firstPoint = route.routePoints.at(0)!;
-      graph.addNode(route.originId.toString(), {[firstPoint.pointId]: firstPoint.distance})
+      graph.addNode(route.originId.toString(), {
+        [firstPoint.pointId]: firstPoint.distance,
+      });
       route.routePoints.forEach((point, i, arr) => {
-        if(!arr.at(i+1)) {
-          graph.addNode(point.pointId.toString(), {[route.destinationId]: point.distanceToDest})
+        if (!arr.at(i + 1)) {
+          graph.addNode(point.pointId.toString(), {
+            [route.destinationId]: point.distanceToDest,
+          });
         } else {
-          graph.addNode(point.pointId.toString(), {[arr[i+1].pointId]: arr[i+1].distance})
+          graph.addNode(point.pointId.toString(), {
+            [arr[i + 1].pointId]: arr[i + 1].distance,
+          });
         }
-      })
+      });
 
-      // route = route._id;
-      // fullRoute[route.id] = [route.originId,...route.routePoints,route.destinationId]
-    })
+      fullRoute[route._id!] = [
+        route.originId.toString(),
+        ...route.routePoints.map(p => p.pointId.toString()),
+        route.destinationId.toString(),
+      ];
+    });
 
-    console.log(fullRoute)
+    const pointsResult = graph.path(
+      points.origin.toString(),
+      points.destination.toString(),
+    );
 
-    console.log(graph.path(points.origin.toString(), points.destination.toString()))
+    const bestRoutePoints: string[] = [];
+
+    if (Array.isArray(pointsResult)) {
+      pointsResult.forEach(el => bestRoutePoints.push(el));
+    }
+
+    let bestRouteId: string = '';
+
+    for (const [routeId, arr] of Object.entries(fullRoute)) {
+      const match = arr.every(
+        (point, index) => point === bestRoutePoints[index],
+      );
+      if (match) {
+        bestRouteId = routeId;
+        break;
+      }
+    }
+
+    if (bestRouteId) {
+      const foundedRoute = routes.find(route => route._id === bestRouteId);
+
+      return foundedRoute;
+    }
+
+    throw new HttpErrors.NotFound('Route not found');;
   }
 }
